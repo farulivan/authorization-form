16bf92857cac8cc67e135381f5020c9698d3fe80
Farul Ivan <farulivan@gmail.com>
2022-10-03 17:35:06 +0700

    finishing project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      password: 'swordfish',
+      authorized: false
+    };
+    this.authorize = this.authorize.bind(this);
+  }
+
+  authorize(e) {
+    const password = e.target.querySelector(
+      'input[type="password"]').value;
+    const auth = password == this.state.password;
+    this.setState({
+      authorized: auth
+    });
+  }
+
+  render() {
+    const login = (
+      <form action="#" onSubmit={this.authorize}>
+        <input 
+          type="password"
+          placeholder="Password"
+        />
+        <input 
+          type="submit" 
+        />
+      </form>
+    );
+    const contactInfo = (
+      <ul>
+        <li>
+          client@example.com
+        </li>
+        <li>
+          555.555.5555
+        </li>
+      </ul>
+    );
+    return (
+      <div id="authorization">
+        {this.state.authorized ? <h1>Contact</h1> : <h1>Enter the Password</h1>}
+        {this.state.authorized ? contactInfo : login}
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Contact />
     </div>
   );
 }
